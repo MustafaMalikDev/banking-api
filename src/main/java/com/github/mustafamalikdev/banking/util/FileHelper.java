f0dfda78b8d761181f320503d5d5dcbebfdeae01
MustafaMalikDev <140mustafa@gmail.com>
2026-09-21 19:34:02 +0100

    fix: create directories in root to avoid docker crashes

--- src/main/java/com/github/mustafamalikdev/banking/util/FileHelper.java
+++ src/main/java/com/github/mustafamalikdev/banking/util/FileHelper.java
@@ -3,22 +3,30 @@ package com.github.mustafamalikdev.banking.util;
 import com.github.mustafamalikdev.banking.models.ErrorCode;
 import com.github.mustafamalikdev.banking.models.entities.TransactionModel;
 import com.github.mustafamalikdev.banking.models.requests.MerchantRequestBody;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class FileHelper {
+
+    private static final String LOG_DIR = "/app/logs";
 
     public FileHelper() { }
 
     public static void writeLog(String contents) {
-        LocalDateTime nowTime = LocalDateTime.now();
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss-SSS"));
 
-        ClassLoader classLoader = FileHelper.class.getClassLoader();
-        File file = new File(classLoader.getResource(".").getFile() + nowTime + ".txt");
-        System.out.println(file.getAbsolutePath());
+        File dir = new File(LOG_DIR);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+
+        File file = new File(dir, timestamp + ".txt");
 
         try {
             if (file.createNewFile()) {
@@ -35,7 +43,6 @@ public class FileHelper {
         LocalDateTime nowTime = LocalDateTime.now();
         MerchantRequestBody merchant = data.getMerchant();
 
-        // TODO replace timestamp
         String formattedLog = String.format(
                 "--------------------------------------------------------------------------------\n" +
                         " SYSTEM TRANSACTION REPORT   \n" +
